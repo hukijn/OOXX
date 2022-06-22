0c51caedb1fc85a1408c808322df79cf1a2883f2
hukijn <102371699+hukijn@users.noreply.github.com>
2022-06-22 16:34:32 +0800

    Create Tfset.h

---
+++ OOXX/Tfset.h
@@ -0,0 +1,16 @@
+#include "position.h"
+int TFset(int mx, int my, int k, int &x, int &y, int bigmap[3][3], int Map[9][3][3]){
+    if((mx > 0 && mx < 300 && my > 0 && my < 270 && bigmap[0][0] == 1) ||
+    (mx > 300 && mx < 600 && my > 0 && my < 270 && bigmap[0][1] == 1) ||
+    (mx > 600 && mx < 900 && my > 0 && my < 270 && bigmap[0][2] == 1) ||
+    (mx > 0 && mx < 300 && my > 270 && my < 530 && bigmap[1][0] == 1) ||
+    (mx > 300 && mx < 600 && my > 270 && my < 530 && bigmap[1][1] == 1) ||
+    (mx > 600 && mx < 900 && my > 270 && my < 530 && bigmap[1][2] == 1) ||
+    (mx > 0 && mx < 300 && my > 530 && my < 800 && bigmap[2][0] == 1) ||
+    (mx > 300 && mx < 600 && my > 530 && my < 800 && bigmap[2][1] == 1) ||
+    (mx > 600 && mx < 900 && my > 530 && my < 800 && bigmap[2][2] == 1)){
+        return position(mx, my, k, x, y, bigmap, Map);
+    }else{
+        return 0;
+    }
+}
