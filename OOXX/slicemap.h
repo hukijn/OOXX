5b2edf931837cc147d6e2dc7b015a2e20f25c2c3
hukijn <102371699+hukijn@users.noreply.github.com>
2022-06-22 16:34:25 +0800

    Update slicemap.h

--- OOXX/slicemap.h
+++ OOXX/slicemap.h
@@ -1,15 +1,15 @@
-void slicemap(int map[9][3][3], int lmap[3][3], int a){
+void slicemap(int Map[9][3][3], int lmap[3][3], int a){
     if(a == 1 || a == 2 || a == 3){
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 3; j++)
-                lmap[i][j] = map[i][a - 1][j];
+                lmap[i][j] = Map[i][a - 1][j];
     }else if(a == 4 || a == 5 || a == 6){
         for(int i = 3; i < 6; i++)
             for(int j = 0; j < 3; j++)
-                lmap[i - 3][j] = map[i][a - 4][j];
+                lmap[i - 3][j] = Map[i][a - 4][j];
     }else{
         for(int i = 6; i < 9; i++)
             for(int j = 0;j < 3; j++)
-                lmap[i - 6][j] = map[i][a - 7][j];
+                lmap[i - 6][j] = Map[i][a - 7][j];
     }
 }
